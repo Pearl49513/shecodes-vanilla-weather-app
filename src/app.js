4190d61303496e657e9dccb32a79a984bd3a5c96
Pearl49513 <casey_dahlia@yahoo.com>
2023-02-13 12:43:03 -0500

    Added time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,32 @@
+//Display time
+function formatDate(timestamp) {
+  //calculate the date
+  let date = new Date(timestamp);
+  //Format hours
+  let hour = date.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  //Format mintues
+  let minute = date.getMinutes();
+  if (minute < 10) {
+    minute = `0${mintues}`;
+  }
+  //Format Weekdays
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  //Return Date
+  return `${day} ${hour}:${minute}`;
+}
+
 // Display Temperature, City, Weather Description, Humidity, Wind
 function displayTemperature(response) {
   console.log(response.data);
@@ -7,11 +36,13 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let precipitationElement = document.querySelector("#precipitation");
+  let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].main;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let weatherApiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
