96d82e4c1595746ae69a9882af99599ce6df2010
Pearl49513 <casey_dahlia@yahoo.com>
2023-02-15 14:49:29 -0500

    Squashed commit of the following:
    commit 82d316b97fd51ca73eb5b461a8fe302eec916a07
    Author: Pearl49513 <casey_dahlia@yahoo.com>
    Date:   Wed Feb 15 14:43:24 2023 -0500
    
        Test
    
        to review and merge
    
    commit 3f27443fc72b7df37f9eab6197b16366517e469e
    Author: Pearl49513 <casey_dahlia@yahoo.com>
    Date:   Wed Feb 15 14:40:01 2023 -0500
    
        Small changes
    
    commit b8a819182c4ed7d85e3d0f0fcff293d19f9bc545
    Author: Pearl49513 <casey_dahlia@yahoo.com>
    Date:   Wed Feb 15 12:19:40 2023 -0500
    
        Added unit conversion
    
    commit 7261390f0dac1f4186878737a1b6f0c2ed8ed7d5
    Author: Pearl49513 <casey_dahlia@yahoo.com>
    Date:   Mon Feb 13 14:54:30 2023 -0500
    
        Added Search Engine with button
    
    commit 04e78f4a6be1ce445ec7800ee4db76b685989ed3
    Author: Pearl49513 <casey_dahlia@yahoo.com>
    Date:   Mon Feb 13 14:01:38 2023 -0500
    
        Added icon from OpenWeatherAPI
    
    commit bea7147aa3d2256ea3503fdb79223ce337c52e26
    Author: Pearl49513 <casey_dahlia@yahoo.com>
    Date:   Mon Feb 13 12:46:29 2023 -0500
    
        added city city js object just in case
    
    commit 4190d61303496e657e9dccb32a79a984bd3a5c96
    Author: Pearl49513 <casey_dahlia@yahoo.com>
    Date:   Mon Feb 13 12:43:03 2023 -0500
    
        Added time
    
    commit 62a4811c6b809c56cb61ae183202ad1850a076ea
    Author: Pearl49513 <casey_dahlia@yahoo.com>
    Date:   Mon Feb 13 12:12:16 2023 -0500
    
        Added JS of city, temp, weather desc, wind, and humidity
    
    commit 33d2bad4884430d50d3ad2f303884601478ab194
    Author: Pearl49513 <casey_dahlia@yahoo.com>
    Date:   Mon Feb 13 11:01:13 2023 -0500
    
        Update index.html
    
    commit 2dcf567e717c14a79a7965d14c0781365e9d4b96
    Author: Pearl49513 <casey_dahlia@yahoo.com>
    Date:   Sat Feb 11 16:17:22 2023 -0500
    
        Added some css
    
    commit 8fc6bb1f46e29a39520bb04c21f8d48f78b88e1a
    Author: Pearl49513 <casey_dahlia@yahoo.com>
    Date:   Sat Feb 11 15:40:41 2023 -0500
    
        Style main elements of the app
    
    commit ac17cbc01be20e7539b3c9a3a2d407d8a8fc1ccc
    Author: Pearl49513 <casey_dahlia@yahoo.com>
    Date:   Sat Feb 11 14:37:43 2023 -0500
    
        Added HTML structure
    
    commit 88721232a1b77fb3cc574d54dc22ea0dd63ca6fe
    Author: Pearl49513 <casey_dahlia@yahoo.com>
    Date:   Sat Feb 11 14:01:51 2023 -0500
    
        Plan

--- src/app.js
+++ src/app.js
@@ -1,1 +1,106 @@
-alert("hello world");
+//Display time
+function formatDate(timestamp) {
+  //calculate the date
+  let date = new Date(timestamp);
+  //Format hours
+  let hour = date.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  //Format mintues
+  let minute = date.getMinutes();
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
+  //Format Weekdays
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  //Return Date
+  return `${day} ${hour}:${minute}`;
+}
+
+// Display Temperature, City, Weather Description, Humidity, Wind
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  //let precipitationElement = document.querySelector("#precipitation");
+  let dateElement = document.querySelector("#date");
+  let weatherIconElement = document.querySelector("#weather-icon");
+
+  celsTemp = response.data.main.temp;
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].main;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  weatherIconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  weatherIconElement.setAttribute("alt", response.data.weather[0].main);
+}
+
+//Detects input value of search engine
+function searchCity(city) {
+  //Weather API Key and URL
+  let weatherApiKey = "88724523008dc9e1be18f6eb6a959b67";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherApiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+//Activate Search button
+function submitCityEvent(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  //User Input being Detected
+  searchCity(cityInputElement.value);
+}
+
+//Activates Fahrenheit Temperature
+function displayFahrTemp(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  let fahrTemp = (celsTemp * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrTemp);
+  //Remove activation of celsius
+  celsLink.classList.remove("active");
+  fahrLink.classList.add("active");
+}
+
+//Activates Celsius Temperature
+function displayCelsTemp(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsTemp);
+  //Remove activation of fahrenheit
+  fahrLink.classList.remove("active");
+  celsLink.classList.add("active");
+}
+
+//Unit Switch Fahrenheit
+let celsTemp = null;
+let fahrLink = document.querySelector("#fahr-link");
+fahrLink.addEventListener("click", displayFahrTemp);
+
+//Unit Switch Celsius
+let celsLink = document.querySelector("#cels-link");
+celsLink.addEventListener("click", displayCelsTemp);
+
+//Search City Event
+let citySearch = document.querySelector("#search-form");
+citySearch.addEventListener("submit", submitCityEvent);
+searchCity("New York");
+
+//test
