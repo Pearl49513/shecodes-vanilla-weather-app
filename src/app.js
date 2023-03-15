471a0a3e137c7f8dd83ae9f95a1342d340df30ab
Pearl49513 <casey_dahlia@yahoo.com>
2023-03-14 22:54:40 -0400

    Trying to get the wind gauge to be fixed

--- src/app.js
+++ src/app.js
@@ -53,13 +53,14 @@ function displayTemperature(response) {
   weatherIconElement.setAttribute("alt", response.data.weather[0].main);
 
   getForecast(response.data.coord);
+  coords = response.data.coord;
 }
 
 //Detects input value of search engine
 function searchCity(city) {
   //Weather API Key and URL
   let weatherApiKey = "88724523008dc9e1be18f6eb6a959b67";
-  let units = "metric";
+  //let units = "metric";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherApiKey}&units=${units}`;
   axios.get(apiUrl).then(displayTemperature);
 }
@@ -78,20 +79,17 @@ function displayFahrTemp(event) {
   //Remove activation of celsius
   celsLink.classList.remove("active");
   fahrLink.classList.add("active");
-  let temperatureElement = document.querySelector("#temperature");
-  let fahrTemp = (celsTemp * 9) / 5 + 32;
+  document.getElementById("wind-unit").hidden = true;
 
-  //Testing something
-  if (units === "metric") {
-    units = "imperial";
-  }
-  allUnits.forEach(function (tempHtml) {
-    let temp = parseInt(tempHtml.innerHTML);
-    let tempH = Math.round((temp * 9) / (5 + 32));
-    tempHtml.innerHTML = tempH;
-  });
-  //Test ended
-  temperatureElement.innerHTML = Math.round(fahrTemp);
+  //newWindUnit.textContent = "mph";
+
+  //let temperatureElement = document.querySelector("#temperature");
+  //let fahrTemp = (celsTemp * 9) / 5 + 32;
+  units = "imperial";
+  let weatherApiKey = "88724523008dc9e1be18f6eb6a959b67";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${weatherApiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayTemperature);
+  //temperatureElement.innerHTML = Math.round(fahrTemp);
 }
 
 //Activates Celsius Temperature
@@ -100,23 +98,21 @@ function displayCelsTemp(event) {
   //Remove activation of fahrenheit
   fahrLink.classList.remove("active");
   celsLink.classList.add("active");
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(celsTemp);
-  //Testing something
-  if (units === "imperial") {
-    units = "metric";
-  }
-  allUnits.forEach(function (tempHtml) {
-    let temp = parseInt(tempHtml.innerHTML);
-    let tempC = Math.round((temp - 32) / (9 / 5));
-    tempHtml.innerHTML = tempC;
-  });
-  //Test ended
+  document.getElementById("wind-unit").hidden = false;
+  //var windUnit = document.getElementById("wind-unit");
+  //var newWindUnit = document.createTextNode("km/h");
+  //windUnit.replaceChild(newWindUnit);
+  //let temperatureElement = document.querySelector("#temperature");
+  //temperatureElement.innerHTML = Math.round(celsTemp);
+  units = "metric";
+  let weatherApiKey = "88724523008dc9e1be18f6eb6a959b67";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${weatherApiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayTemperature);
 }
 
 //Weather Forecast Function================================================================================================================
 function displayForecast(response) {
-  console.log(response.data);
+  //console.log(response.data);
   let dailyForecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast-columns");
   let forecastHTML = `<div class="row" style="border: solid">`;
@@ -141,7 +137,7 @@ function displayForecast(response) {
                   width="60"
                 />
                 <div class="weather-forecast-temp">
-                  <span class="weather-forecast-temp">${maxi}° </span>
+                  <span class="weather-forecast-temp">${maxi}°</span>
                   <span class="weather-forecast-temp">${mini}°</span>
                 </div>
               </div>`;
@@ -154,7 +150,7 @@ function displayForecast(response) {
 function getForecast(coordinates) {
   //console.log(coordinates);
   let weatherApiKey = "88724523008dc9e1be18f6eb6a959b67";
-  let units = "metric";
+  //let units = "metric";
   let apiForecastUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${weatherApiKey}&units=${units}`;
   //console.log(apiForecastUrl);
   axios.get(apiForecastUrl).then(displayForecast);
@@ -168,16 +164,18 @@ function formatDay(timestamp) {
 }
 
 //Weather Search===================================================================================================
-//Unit Switch Fahrenheit
-let celsTemp = null;
+//Unit Switch
+let units = "metric";
+let coords = null;
+//let celsTemp = null;
+
+//Fahrenheit
 let fahrLink = document.querySelector("#fahr-link");
 fahrLink.addEventListener("click", displayFahrTemp);
 
-//Unit Switch Celsius
+//Celsius
 let celsLink = document.querySelector("#cels-link");
 celsLink.addEventListener("click", displayCelsTemp);
-
-let allUnits = document.querySelectorAll(".weather-forecast-temp");
 
 //Search City Event
 let citySearch = document.querySelector("#search-form");
